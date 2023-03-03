db5b79ccef5120d9870f66cd3c7625e28ad52aab
Maria Eduarda Couto Grossi <maria.grossi1@estudante.ufla.br>
2023-03-03 11:06:16 -0300

    conexao back-front login

--- back-end/servidor.js
+++ back-end/servidor.js
@@ -5,18 +5,17 @@ import eventoRouter from "./routes/evento.routes.js"
 import casaRouter from "./routes/casaArtistica.routes.js"
 import admRouter from "./routes/adm.routes.js"
 
-const servidor = express() //retorna o servidor pronto de express - instancia o pacote importado
+const servidor = express() 
 
 servidor.use(express.json())
 servidor.use(express.urlencoded({extended: true}))
-servidor.use("/desenhista", desenhistaRouter) //toda vez que chamar /desenhista chama o roteador responsável
+servidor.use("/desenhista", desenhistaRouter) 
 servidor.use("/desenho", desenhoRouter)
 servidor.use("/evento", eventoRouter)
 servidor.use("/casa", casaRouter)
 servidor.use("/adm", admRouter)
 
-servidor.listen(3001, servico) //servidor ouve de uma certa porta lógica - nunca criar abaixo de 1000 (convenção)
-//passa como parametro servico que indica o que acontecerá quando a requisição chegar na porta 3000 (nosso sistema)
+servidor.listen(3001, servico) 
 
 function servico(){
     console.log("Servidor rodando...") //mensagem quando chega na porta 
